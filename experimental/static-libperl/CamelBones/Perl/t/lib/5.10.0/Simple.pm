56130789f9a23b4c7da54b081d288e0ee4fc872e
shermpendley <shermpendley@af993884-d23c-0410-a8d8-c63a106ea042>
2008-01-20 20:06:46 +0000

    Added Perl 5.10, and first hack at a build target for it.

---
+++ experimental/static-libperl/CamelBones/Perl/t/lib/5.10.0/Simple.pm
@@ -0,0 +1,24 @@
+package Simple;
+
+use vars qw( $VERSION );
+$VERSION = '0.01';
+
+use strict;
+
+1;
+
+__END__
+
+=head1 NAME
+
+Simple - Perl extension for blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for Simple.
+
+=head1 AUTHOR
+
+A. U. Thor, a.u.thor@a.galaxy.far.far.away
+
+=cut
