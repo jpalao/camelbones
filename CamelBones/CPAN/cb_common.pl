01b9dfcc0016757b6ee91874cd37057576645cea
Jose Palao <jose@palao.com>
2016-05-21 13:59:17 +0200

    Makefile.PL fixes

--- CamelBones/CPAN/cb_common.pl
+++ CamelBones/CPAN/cb_common.pl
@@ -13,17 +13,17 @@ $CamelBones = abs_path($CamelBones);
 $CamelBonesPath = abs_path($CamelBonesPath);
 
 our %opts = (
-    VERSION           => '1.1.2',
-    CCFLAGS 		  => "-arch i386 -arch x86_64",
+    VERSION           => '1.2.0',
+    CCFLAGS           => "-arch i386 -arch x86_64 -Wall",
     PREREQ_PM         => {},
 
-    AUTHOR         	  => 'Sherm Pendley <sherm.pendley@gmail.com>',
+    AUTHOR            => 'Sherm Pendley <sherm.pendley@gmail.com>',
 
     XSOPT             => "-typemap $CamelBones/Resources/typemap",
 
-    LIBS              => [ '-lobjc', '-arch i386', '-arch x86_64' ],
+    LIBS              => [ '-lobjc'],
     INC               => "-F$CamelBonesPath ",
-    dynamic_lib         => {
+    dynamic_lib       => {
                         'OTHERLDFLAGS' =>
                             "-arch i386 -arch x86_64  -framework Foundation -framework AppKit -framework CamelBones -F/System/Library/Frameworks -F$CamelBonesPath "
                         },
