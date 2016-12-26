63dc20ef8d70651aa403c076bfa91a23b210b3ad
Jose Palao <jose@palao.com>
2016-12-26 19:16:34 +0100

    initial CoreGraphics support

---
+++ CamelBones/CPAN/lib/CamelBones/CoreGraphics.pm
@@ -0,0 +1,24 @@
+use strict;
+use warnings;
+
+package CamelBones::CoreGraphics;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our $VERSION = '1.2.0';
+our @EXPORT = qw(
+			  );
+our @EXPORT_OK = qw(
+	CGMainDisplayID	
+	CGDisplayGetDrawingContext
+	CGDisplayCapture
+		CGPointMake CGAffineTransformMake CGPathGetBoundingBox
+				   );
+our %EXPORT_TAGS = (
+    'All'		=> [@EXPORT_OK],
+);
+
+require XSLoader;
+XSLoader::load('CamelBones::CoreGraphics');
+
+1;
