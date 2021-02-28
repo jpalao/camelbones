7a965a3ef76c2fddb06eaf236e447cba4cd4b334
jpalao <jpalao@personasytecnicas.com>
2021-02-28 17:13:09 +0100

    check exit code, document, refactor

--- CamelBones/install_macosx.sh
+++ CamelBones/install_macosx.sh
@@ -17,7 +17,15 @@ export CAMELBONES_BUILD_CONFIGURATION='Debug'
 export CAMELBONES_TARGET='macosx'
 export INSTALL_CAMELBONES_FRAMEWORK=1
 export OVERWRITE_CAMELBONES_FRAMEWORK=1
-export XCODE_SCHEME='macosx'
+
+check_exit_code() {
+  if [ $? -ne 0 ]; then
+    echo "Failed to build CamelBones for $CAMELBONES_TARGET"
+    exit $?
+  fi
+}
+
+# build_libffi #
 
 # $ xcodebuild -list
 # Command line invocation:
@@ -33,7 +41,8 @@ export XCODE_SCHEME='macosx'
 #         Debug
 #         Release
 # 
-#     If no build configuration is specified and -scheme is not passed then "Release" is used.
+#     If no build configuration is specified and -scheme is not passed then 
+#     "Release" is used.
 # 
 #     Schemes:
 #         libffi-iphoneos
@@ -41,26 +50,62 @@ export XCODE_SCHEME='macosx'
 #         libffi-macosx
 
 build_libffi() {
-  pushd ./libffi-3.2.1
-  xcodebuild -scheme libffi-"$CAMELBONES_TARGET"
-  popd
+    pushd ./libffi-3.2.1
+    xcodebuild -scheme libffi-"$CAMELBONES_TARGET"
+    popd
 }
 
+# $ xcodebuild -list
+# Command line invocation:
+#     /Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild -list
+# 
+# Information about project "CamelBones":
+#     Targets:
+#         macosx
+#         iphone
+#         All
+# 
+#     Build Configurations:
+#         Debug
+#         Release
+# 
+#     If no build configuration is specified and -scheme is not passed then "Release" is used.
+# 
+#     Schemes:
+#         All
+#         iphoneos
+#         iphonesimulator
+#         macosx
+# 
+
+build_camelbones() {
+    xcodebuild  ARCHS="$ARCHS" PERL_INCLUDE_DIR="$PERL_INCLUDE_DIR" \
+    ONLY_ACTIVE_ARCH=NO -allowProvisioningUpdates -scheme "$CAMELBONES_TARGET"
+}
+
+build_extensions() {
+    pushd CPAN
+    make distclean
+    perl Makefile.PL
+    make
+    make test
+    popd
+}
+
+
+######################################################
+
 build_libffi
+check_exit_code
 
-#  CamelBones.framework
-#  CamelBonesiOS.framework
-#  CamelBonesiOS.framework simulator
+build_camelbones
+check_exit_code
 
-xcodebuild  ARCHS="$ARCHS" PERL_INCLUDE_DIR="$PERL_INCLUDE_DIR" ONLY_ACTIVE_ARCH=NO \
- -allowProvisioningUpdates -scheme "$XCODE_SCHEME"
-
-# if INSTALL_CAMELBONES_FRAMEWORK
+# TODO? if INSTALL_CAMELBONES_FRAMEWORK
 perl -w ./install_framework.pl
+check_exit_code
 
-cd CPAN
-make distclean
-perl Makefile.PL
-make
-make test
+build_extensions
+check_exit_code
+
 
